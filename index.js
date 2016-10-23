8ab88d18df9a9d029f2221160bceaf0582805edf
marchefk <jackiewicz.ewa@wp.pl>
2016-10-23 17:05:58 +0100

    changes

--- index.js
+++ index.js
@@ -3,33 +3,45 @@ var desc = "";
 var link = "";
 
 function getInput() {
-    var phrase = $("#input").val();
-    return phrase;
+  var phrase = $("#input").val();
+  return phrase;
 }
 
 function getResults() {
-    var value = getInput();
-    console.log(value);
-    var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + value + "&callback=?";
-    console.log(url);
-    $.getJSON(url, function(data) {
-        console.log(data);
-        for (var i = 0; i < 10; i++) {
-            title = data.query.search[i].title;
-            desc = data.query.search[i].snippet;
-            link = "https://en.wikipedia.org/wiki/" + title;
-            console.log(link);
-            $("#p" + i).addClass("altered");
-            $("#p" + i).html("<h3>" + title + "</h3>" + desc + "...");
-            $("#a" + i).attr('href', link);
-        }
-    })
+  var value = getInput();
+  console.log(value);
+  var url = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${value}&callback=?`;
+  console.log(url);
+  $.getJSON(url, function(data) {
+    console.log(data);
+    var validator = data.query.search[0];
+    console.log(validator);
+    if (validator === undefined) {
+      $("#notfound").show();
+      $("#notfound").html("No results found.");
+      for (var i = 0; i < 10; i++) {
+        $("#p" + i).hide();
+      }
+    } else {
+      $("#notfound").hide();
+      for (var i = 0; i < 10; i++) {
+        title = data.query.search[i].title;
+        desc = data.query.search[i].snippet;
+        link = `https://en.wikipedia.org/wiki/${title}`;
+        console.log(link);
+        $("#p" + i).show();
+        $("#p" + i).addClass("altered");
+        $("#p" + i).html(`<h3>${title}</h3>${desc}...`);
+        $("#a" + i).attr('href', link);
+      }
+    }
+  })
 }
 $("#search").on("click", function() {
-    getResults()
+  getResults()
 })
 $("#input").keyup(function(event) {
-    if (event.keyCode == 13) {
-        getResults();
-    }
+  if (event.keyCode == 13) {
+    getResults();
+  }
 });
