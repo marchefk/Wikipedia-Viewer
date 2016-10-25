981973713e3bb0e8130a393b8523cc8fe6eb3a25
kamilant <kamil.antropik@eyedea.io>
2016-10-26 00:36:22 +0200

    [#1] Code cleanup and refactor

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var title = "";
-var desc = "";
-var link = "";
+let title = "",
+    desc = "",
+    link = "";
 
 function getInput() {
   var phrase = $("#input").val();
@@ -9,14 +9,10 @@ function getInput() {
 
 function getResults() {
   var value = getInput();
-  console.log(value);
   var url = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${value}&callback=?`;
-  console.log(url);
   $.getJSON(url, function(data) {
-    console.log(data);
     var validator = data.query.search[0];
-    console.log(validator);
-    if (validator === undefined) {
+    if (!validator) {
       $("#notfound").show();
       $("#notfound").html("No results found.");
       for (var i = 0; i < 10; i++) {
@@ -28,7 +24,6 @@ function getResults() {
         title = data.query.search[i].title;
         desc = data.query.search[i].snippet;
         link = `https://en.wikipedia.org/wiki/${title}`;
-        console.log(link);
         $("#p" + i).show();
         $("#p" + i).addClass("altered");
         $("#p" + i).html(`<h3>${title}</h3>${desc}...`);
